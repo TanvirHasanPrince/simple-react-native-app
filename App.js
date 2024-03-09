1062e8db809924f8a78506e06d4278e042184879
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-09 10:53:20 +0600

    [Done]- Intro section

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing</Text>
+      <View style={styles.row}>
+        <Image
+          source={require("./assets/octocat-1709958392792.png")}
+          style={styles.avatar}
+        />
+        <Text style={[styles.textContainer, styles.textDistance]}>
+          Tanvir Hasan Prince
+          {"\n"}
+          <Text style={{ color: "blue" }}>@tanvir.hasan.prince</Text>
+          {"\n"}
+          Joined on 9th March 2024
+        </Text>
+      </View>
+      <Text style={styles.textContainer}>
+        I'm a passionate product engineer who thrives on solving real-world
+        problems with elegant code solutions. With a keen eye for detail and a
+        knack for innovation, I specialize in developing web applications using
+        a variety of modern technologies and frameworks.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +30,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+    borderRadius: 50 / 2,
+  },
+  textContainer: {
+    flex: 1,
+    marginLeft: 10,
+    alignItems: "center",
+  },
+  textDistance: {
+    lineHeight: 30,
   },
 });
