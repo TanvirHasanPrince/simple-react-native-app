31dd37c12de81f6f996fd26cb4cec4a378ffe2c1
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-03-09 13:02:54 +0600

    [Done]- Stats

--- App.js
+++ App.js
@@ -23,6 +23,41 @@ export default function App() {
         knack for innovation, I specialize in developing web applications using
         a variety of modern technologies and frameworks.
       </Text>
+
+      <View style={styles.stats}>
+        <Text style={[styles.textContainer, styles.textDistance]}>
+          Repos
+          {"\n"}
+          <Text style={{ fontWeight: "bold", fontSize: 30 }}>148</Text>
+        </Text>
+
+        <Text style={[styles.textContainer, styles.textDistance]}>
+          Followers
+          {"\n"}
+          <Text style={{ fontWeight: "bold", fontSize: 30 }}>32</Text>
+        </Text>
+
+        <Text style={[styles.textContainer, styles.textDistance]}>
+          Following
+          {"\n"}
+          <Text style={{ fontWeight: "bold", fontSize: 30 }}>20</Text>
+        </Text>
+      </View>
+
+      <View style={styles.row}>
+        <Image
+          source={require("./assets/octocat-1709958392792.png")}
+          style={styles.avatar}
+        />
+        <Text style={[styles.textContainer, styles.textDistance]}>
+          Tanvir Hasan Prince
+          {"\n"}
+          <Text style={{ color: "blue" }}>@tanvir.hasan.prince</Text>
+          {"\n"}
+          Joined on 9th March 2024
+        </Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -33,6 +68,17 @@ const styles = StyleSheet.create({
     marginTop: 50,
     flex: 1,
     backgroundColor: "white",
+  },
+  stats: {
+    marginTop: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: 10,
+    paddingRight: 0,
+    backgroundColor: "#d3d3d3",
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
   },
   row: {
     flexDirection: "row",
